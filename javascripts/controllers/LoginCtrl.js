ceb781be9766163b242c8efea9d07e1e9fa7cfef
Matt Hensley <matt.hensley777@gmail.com>
2017-11-15 19:56:49 -0600

    Update LoginCtrl.js

--- javascripts/controllers/LoginCtrl.js
+++ javascripts/controllers/LoginCtrl.js
@@ -2,14 +2,10 @@
 
 app.controller("LoginCtrl", function($location, $rootScope, $scope, AuthService){
   $scope.controller = "LoginCtrl";
-    console.log("hello from Login");
     $scope.authenticate = () => {
     AuthService.authenticateGoogle().then((result) =>{
-      console.log("hello");
       $rootScope.uid = result.user.uid;
-      console.log("result", result.user.uid);
       $scope.$apply(() =>{
-        console.log("inside scope apply");
         $location.url("/contacts/new");
       });
     }).catch((err) =>{
